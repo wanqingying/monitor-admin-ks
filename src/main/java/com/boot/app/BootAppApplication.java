94019607d421c11ea0788989f11931321df0ca37
wanqingying <wanqingying@kuaishou.com>
2023-12-21 21:03:27 +0800

    init 3 boot-admin-server

--- src/main/java/com/boot/app/BootAppApplication.java
+++ src/main/java/com/boot/app/BootAppApplication.java
@@ -1,18 +1,17 @@
 package com.boot.app;
 
+import de.codecentric.boot.admin.server.config.AdminServerHazelcastAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-@RestController
+//@RestController
 @SpringBootApplication
+@EnableAdminServer
 public class BootAppApplication {
 
-    @GetMapping("/foo")
-    public String foo() {
-        return "bar v4";
-    }
     public static void main(String[] args) {
         SpringApplication.run(BootAppApplication.class, args);
     }
